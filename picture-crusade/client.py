7961aa8a56ae5d18b701091fa53f2a24763cfdb7
kekwait <71540899+kekwait@users.noreply.github.com>
2021-06-13 22:06:06 +0700

    Update client.py

--- picture-crusade/client.py
+++ picture-crusade/client.py
@@ -1,9 +1,4 @@
 import requests
-try:
- r = requests.get('http://localhost:5000/')
- print(r.status_code)
- if(r.status_code!=200):
-  exit(1)
- print(r.text)
-except:
- exit(1) 
+r = requests.get('http://localhost:5000/')
+print(r.status_code)
+print(r.text)
