da6bf101a77d9afd86e9d3ecd9a9a1decb9a2379
kekwait <71540899+kekwait@users.noreply.github.com>
2021-06-13 21:45:04 +0700

    Add files via upload

--- picture-crusade/app.py
+++ picture-crusade/app.py
@@ -1,75 +1,77 @@
 import os
-from flask import Flask, request, redirect, url_for, render_template
-from werkzeug.utils import secure_filename
+
+from flask import Flask, request, render_template,redirect,url_for
 from flask_wtf import FlaskForm, RecaptchaField
-from PIL import Image, ImageDraw
+from wtforms import RadioField
+from werkzeug.utils import secure_filename
+from PIL import Image, ImageChops
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+from numpy import transpose
+
+UPLOAD_FOLDER = './static/images/'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+app = Flask(__name__)
 
 
-UPLOAD_FOLDER = 'images'
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
-
-
-app = Flask(__name__, template_folder="templates", static_folder="images")
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config["SECRET_KEY"] = "eifwkuwebfwejubfwj23123857y28wad"
-app.config["RECAPTCHA_PUBLIC_KEY"] = "6LdHWLkaAAAAAFJioRgnPe-YNl4xxUhNaLTkCZno"
-app.config["RECAPTCHA_PRIVATE_KEY"] = "6LdHWLkaAAAAANYhxdrrw7ujYb-3g4aZGOZcWcN8"
-colours = ['Red', 'Blue', 'Black', 'Orange']
-
-class ReCaptcha(FlaskForm):
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.config['SECRET_KEY']='6LcRtxQbAAAAANQ_wEKDLDmpFmUuFE4bEzB0B293'
+app.config['RECAPTCHA_USE_SSL']= False
+RECAPTCHA_PUBLIC_KEY='6Le59hobAAAAAFdSpa8vEvHEByDV7wIwL619sRzY'
+RECAPTCHA_PRIVATE_KEY='6Le59hobAAAAAK10crhmg7kIprMx6HhFySkPNyEf'
+app.config['RECAPTCHA_SECRET_KEY'] = '6Le59hobAAAAAK10crhmg7kIprMx6HhFySkPNyEf'
+app.config['RECAPTCHA_PUBLIC_KEY'] = RECAPTCHA_PUBLIC_KEY
+app.config['RECAPTCHA_PRIVATE_KEY'] = RECAPTCHA_PRIVATE_KEY
+class Fields(FlaskForm):
+    choice = RadioField(
+        'Выберите опцию:', choices=[('1', 'Left/Right'), ('2', 'Up/down')],default='1')
     recaptcha = RecaptchaField()
-
-@app.route('/img/<filename>+<color>')
-def uploaded_file(filename, color):
-    try:
-        original = Image.open("./images/"+filename)
-        original_array = np.array(original)
-        original_array_r = original_array[:,:,0].flatten()
-        original_array_g = original_array[:, :, 1].flatten()
-        original_array_b = original_array[:, :, 2].flatten()
-        original_array = np.array([original_array_r,original_array_g,original_array_b])
-        original_array = original_array.transpose()
-        plt.hist(original_array)
-        plt.savefig("./images/plots/original_plot/"+filename+".png")
-        originalplot_path = str('../images/plots/original_plot/'+filename+".png")
-        new_img = original.convert('RGB')
-        draw = ImageDraw.Draw(new_img)
-        x,y = new_img.size
-        draw.rectangle((x-int(x/4), int(y/4),int(x/4),int((x-y)/5)+int(y/4)), fill=color)
-        draw.rectangle(((int((x-y)/5)+int(y/4))*2, int(y/12), x-(int((x-y)/5)+int(y/4))*2, int(y/1.2)), fill=color)
-        new_array = np.array(new_img)
-        new_array_r = new_array[:, :, 0].flatten()
-        new_array_g = new_array[:, :, 1].flatten()
-        new_array_b = new_array[:, :, 2].flatten()
-        new_array = np.array([new_array_r, new_array_g, new_array_b])
-        new_array = new_array.transpose()
-        plt.hist(new_array)
-        plt.savefig('./images/plots/new_plot/'+ filename+".png")
-        new_img.save("./images/new"+filename)
-        new_filename = str("../images/new"+filename)
-        newplot_path = str('../images/plots/new_plot/'+filename+".png")
-    except FileNotFoundError:
-        print("Файл не найден")
-    return render_template('/image.html', old_image=filename, new_image =new_filename,colour = color, old_plot=originalplot_path, new_plot=newplot_path)
-
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
-    form = ReCaptcha()
-    if request.method == 'POST':
+    form = Fields()
+    if request.method == 'POST' and form.validate_on_submit():
         file = request.files['file']
-        if file and allowed_file(file.filename) and form.validate_on_submit():
+        choice = int(form.choice.data)
+        if file and (file.content_type.rsplit('/', 1)[1] in ALLOWED_EXTENSIONS).__bool__():
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            select = request.form['colour']
-            return redirect(url_for('uploaded_file', filename=filename, color=select))
-    return render_template('index.html', colours = colours, form = form)
+            file.save(app.config['UPLOAD_FOLDER'] + filename)
+            return redirect(url_for('uploaded_file', filename=filename, option=choice))
+    return render_template('index.html', form=form)
+
+
+@app.route('/<filename>+<int:option>')
+def uploaded_file(filename,option):
+    path = './static/images/' + filename
+    img = Image.open(path)
+    x,y = img.size
+    if option == 2:
+        img=ImageChops.offset(img, 0, y//2)
+    else:
+        img = ImageChops.offset(img, x//2, 0)
+    new_path = './static/images/new'+ filename
+    img.save(new_path)
+    img_arr = np.array(img)
+    img_arr_graph = np.array([img_arr[:,:,0].flatten(),img_arr[:,:,1].flatten(),img_arr[:,:,2].flatten()]).transpose()
+    print(img_arr_graph)
+    plt.hist(img_arr_graph)
+    graph_path = "./static/plots/"+filename
+    plt.savefig(graph_path)
+    plt.close()
+    return render_template('image.html', img = path, option=option, new_img=new_path, graph = graph_path)
+
+try:
+    r = requests.get('http://localhost:5000/')
+    print(r.status_code)
+    if(r.status_code!=200):
+        exit(1)
+    print(r.text)
+except:
+    exit(1)
 
 if __name__ == '__main__':
-    app.run(debug=False, host='127.0.0.1',port=5000)
+    app.run(debug=True)
