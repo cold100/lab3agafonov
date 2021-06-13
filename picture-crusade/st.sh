4304f4fb703aacc92c52dff194d2f9c766b2f157
kekwait <71540899+kekwait@users.noreply.github.com>
2021-06-13 21:46:41 +0700

    Update st.sh

--- picture-crusade/st.sh
+++ picture-crusade/st.sh
@@ -1,6 +1,10 @@
 gunicorn --bind 127.0.0.1:5000 wsgi:app & APP_PID=$!
-sleep 5 
+sleep 25
+echo start client
+python3 client.py
+APP_CODE=$?
+sleep 5
 echo $APP_PID 
-kill -TERM $APP_PID 
-echo process gunicorns kills 
-exit 0
+kill -TERM $APP_PID
+echo app code $APP_CODE
+exit $APP_CODE
